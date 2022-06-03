2723e7e35eaca1b02762292b2c61e83f567b03d3
Ahmad Malik <arma7x@live.com>
2022-06-03 22:23:05 +0800

    manage user UI

--- resources/views/livewire/admin/manage-user/user-list.blade.php
+++ resources/views/livewire/admin/manage-user/user-list.blade.php
@@ -1,10 +1,37 @@
 <div>
-    <div class="row">
-    @foreach ($user_list as $user)
-        <div>{{ $user->id }}, {{ $user->name }}, {{ $user->email }}, {{ $user->access_level }}, {{ $user->read_permission }}, {{ $user->write_permission }}</div>
-        <div>{{ $user->toJson() }}</div>
-        <button class="btn btn-sm btn-info" onClick="onSetCursor({{ $user->toJson() }})">Set Cursor</button>
-    @endforeach
+    <div class="table-responsive">
+        <table class="table table-sm">
+            <thead>
+                <td>Id</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Email Verified At</td>
+                <td>Access Level</td>
+                <td>Read Permission</td>
+                <td>Write Permission</td>
+                <td>Created At</td>
+                <td>Updated At</td>
+                <td>Action</td>
+            </thead>
+            <tbody>
+                @foreach ($user_list as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ is_null($user->email_verified_at) || !$user->email_verified_at ? "!" : "✔" }}</td>
+                    <td>{{ $user->access_level }}</td>
+                    <td>{{ $user->read_permission }}</td>
+                    <td>{{ $user->write_permission }}</td>
+                    <td>{{ $user->created_at }}</td>
+                    <td>{{ $user->updated_at }}</td>
+                    <td>
+                        <button class="btn btn-sm btn-info" onClick="onSetCursor({{ $user->toJson() }})">Set Cursor</button>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
     {{ $user_list->links() }}
     <div class="mt-2">

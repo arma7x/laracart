2438defcb7825f193bfa81a63084fc2a31c8281c
Ahmad Malik <arma7x@live.com>
2022-06-03 16:23:33 +0800

    create Admin/ManageUser controller

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->middleware('alp:access_level@0,read,write')->group(function () {
+    Route::get('/users', [App\Http\Controllers\Admin\ManageUser::class, 'index']);
+});

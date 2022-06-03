a3c7389c0c3f8515050dbc152ce10e01d726896a
Ahmad Malik <arma7x@live.com>
2022-06-03 22:48:58 +0800

    add vue date component

---
+++ resources/js/components/LocalDateComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <span>{{ toLocal(date) }}</span>
+</template>
+
+<script>
+    export default {
+        props: {
+            date: String
+        },
+        methods: {
+            toLocal: (date) => {
+                return new Date(date).toLocaleString()
+            }
+        },
+        mounted() {}
+    }
+</script>
--- resources/views/livewire/admin/manage-user/user-list.blade.php
+++ resources/views/livewire/admin/manage-user/user-list.blade.php
@@ -5,10 +5,10 @@
                 <td>Id</td>
                 <td>Name</td>
                 <td>Email</td>
-                <td>Email Verified At</td>
                 <td>Access Level</td>
                 <td>Read Permission</td>
                 <td>Write Permission</td>
+                <td>Email Verified At</td>
                 <td>Created At</td>
                 <td>Updated At</td>
                 <td>Action</td>
@@ -19,12 +19,16 @@
                     <td>{{ $user->id }}</td>
                     <td>{{ $user->name }}</td>
                     <td>{{ $user->email }}</td>
-                    <td>{{ is_null($user->email_verified_at) || !$user->email_verified_at ? "!" : "✔" }}</td>
-                    <td>{{ $user->access_level }}</td>
-                    <td>{{ $user->read_permission }}</td>
-                    <td>{{ $user->write_permission }}</td>
-                    <td>{{ $user->created_at }}</td>
-                    <td>{{ $user->updated_at }}</td>
+                    <td class="text-center">{{ $user->access_level }}</td>
+                    <td class="text-center">{{ $user->read_permission }}</td>
+                    <td class="text-center">{{ $user->write_permission }}</td>
+                    @if (is_null($user->email_verified_at) || !$user->email_verified_at)
+                    <td class="text-center">!</td>
+                    @else
+                    <td><local-date date="{{ $user->email_verified_at }}"></local-date></td>
+                    @endif
+                    <td><local-date date="{{ $user->created_at }}" /></td>
+                    <td><local-date date="{{ $user->updated_at }}" /></td>
                     <td>
                         <button class="btn btn-sm btn-info" onClick="onSetCursor({{ $user->toJson() }})">Set Cursor</button>
                     </td>

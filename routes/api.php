f18d266c69f34480ce68dbad43b76a3e0e32b304
Ahmad Malik <arma7x@live.com>
2022-06-04 13:34:55 +0800

    [WIP]: sanctum

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/ping', function (Request $request) {
+    $name = request()->query('name') ?: 'World';
+    return ['php' => "Hello $name from PHP v" . PHP_VERSION];
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
 });

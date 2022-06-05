6f051f1f9dd9993cb44043bcafd4b6719adce3c8
Ahmad Malik <arma7x@live.com>
2022-06-05 22:23:02 +0800

    use Auth Facade

--- app/Rules/MatchOldPassword.php
+++ app/Rules/MatchOldPassword.php
@@ -4,13 +4,14 @@ namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class MatchOldPassword implements Rule
 {
 
     public function passes($attribute, $value)
     {
-        return Hash::check($value, auth()->user()->password);
+        return Hash::check($value, Auth::user()->password);
     }
 
     public function message()

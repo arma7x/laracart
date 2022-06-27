29e7b98b800285c1c7bda59131a9fce657b52dc4
Ahmad Malik <arma7x@live.com>
2022-06-28 01:33:31 +0800

    DONE: Firebase user custom claims

--- app/Http/Livewire/Admin/ManageFirebaseUser/UserList.php
+++ app/Http/Livewire/Admin/ManageFirebaseUser/UserList.php
@@ -12,6 +12,8 @@ class UserList extends Component
     use AuthorizesRequests;
 
     public $user;
+    public $claims = [];
+
     public $search;
 
     protected $queryString = [
@@ -30,29 +32,33 @@ class UserList extends Component
                 $this->user = null;
             } else if (trim($this->search)[0] == '+') {
                 $this->user = Firebase::auth()->getUserByPhoneNumber(trim($this->search))->jsonSerialize();
+                $this->claims = $this->getCustomUserClaims($this->user['uid']);
             } else if (Validator::make(['email' => trim($this->search)], ['email' => 'required|email'])->fails() === false) {
                 $this->user = Firebase::auth()->getUserByEmail(trim($this->search))->jsonSerialize();
+                $this->claims = $this->getCustomUserClaims($this->user['uid']);
             } else {
                 $this->user = Firebase::auth()->getUser(trim($this->search))->jsonSerialize();
+                $this->claims = $this->getCustomUserClaims($this->user['uid']);
             }
         } catch (\Kreait\Firebase\Exception\Auth\UserNotFound $e) {
             $this->user = $e->getMessage();
+            $this->claims = [];
         } catch(\Exception $e) {
             $this->user = null;
+            $this->claims = [];
         }
     }
 
-    // TODO
-    private function getCustomUserClaims($uid)
+    public function getCustomUserClaims($uid)
     {
         $this->authorize('manage-user');
         return Firebase::auth()->getUser($uid)->customClaims;
     }
 
-    // TODO
     public function setCustomUserClaims($uid, $claims = [])
     {
         $this->authorize('manage-user');
+        Firebase::auth()->setCustomUserClaims($uid, COUNT($claims) === 0 ? null : $claims);
     }
 
     public function disableUser($uid)
--- resources/views/livewire/admin/manage-firebase-user/user-list.blade.php
+++ resources/views/livewire/admin/manage-firebase-user/user-list.blade.php
@@ -17,53 +17,82 @@
                 <div class="modal-body">
                     @if ($user['email'] !== null)
                     <div class="d-flex justify-content-between mb-3">
-                        <h5 style="margin-bottom: -2px!important;">Email:</h5>
+                        <h5>Email:</h5>
                         <h5 style="font-weight:bold;">{{ $user['email'] }}[{{ $user['emailVerified'] ? __('Verified') : __('Unverified') }}]</h5>
                     </div>
                     @endif
                     @if ($user['displayName'] !== null)
                     <div class="d-flex justify-content-between mb-3">
-                        <h5 style="margin-bottom: -2px!important;">Display Name:</h5>
+                        <h5>Display Name:</h5>
                         <h5 style="font-weight:bold;">{{ $user['displayName'] }}</h5>
                     </div>
                     @endif
                     @if ($user['phoneNumber'] !== null)
                     <div class="d-flex justify-content-between mb-3">
-                        <h5 style="margin-bottom: -2px!important;">Phone Number:</h5>
+                        <h5>Phone Number:</h5>
                         <h5 style="font-weight:bold;"> {{ $user['phoneNumber'] }}</h5>
                     </div>
                     @endif
                     <div class="d-flex justify-content-between mb-3">
-                        <h5 style="margin-bottom: -2px!important;">Created At:</h5>
+                        <h5>Created At:</h5>
                         <h5 style="font-weight:bold;"><span class="user-date" data-date="{{ $user['metadata']->createdAt->format(DateTimeInterface::RFC3339) }}"></span></h5>
                     </div>
                     <div class="d-flex justify-content-between mb-3">
-                        <h5 style="margin-bottom: -2px!important;">Last Login At:</h5>
+                        <h5>Last Login At:</h5>
                         <h5 style="font-weight:bold;"><span class="user-date" data-date="{{ $user['metadata']->lastLoginAt->format(DateTimeInterface::RFC3339) }}"></span></h5>
                     </div>
                     <div class="d-flex justify-content-between mb-3">
-                        <h5 style="margin-bottom: -2px!important;">Last Refresh At:</h5>
+                        <h5>Last Refresh At:</h5>
                         <h5 style="font-weight:bold;"><span class="user-date" data-date="{{ $user['metadata']->lastRefreshAt->format(DateTimeInterface::RFC3339) }}"></span></h5>
                     </div>
                     <div class="d-flex justify-content-between mb-3">
-                        <h5 style="margin-bottom: -2px!important;">Tokens Valid After Time:</h5>
+                        <h5>Tokens Valid After Time:</h5>
                         <h5 style="font-weight:bold;"><span class="user-date" data-date="{{ $user['tokensValidAfterTime'] }}"></span></h5>
                     </div>
                     <div class="d-flex justify-content-between mb-3">
-                        <h5 style="margin-bottom: -2px!important;">Provider:</h5>
+                        <h5>Provider:</h5>
                         <h5 style="font-weight:bold;">{{ strtoupper($user['providerData'][0]->providerId) }}</h5>
                     </div>
+                    <h5>Custom Claims({{ COUNT($claims) }}):</h5>
+                    <div class="d-flex justify-content-between mb-3">
+                        <form id="claimsForm" class="container m-0 p-0" onsubmit="return false">
+                        @foreach($claims as $key => $value)
+                            <div class="row m-0 p-0 mb-2">
+                                <div class="col m-0 p-0">
+                                    <input name="claims_key" class="form-control" value="{{ $key }}" placeholder="{{ __('Key') }}">
+                                </div>
+                                <div class="col m-0 p-0 mx-2">
+                                    <input name="claims_value" class="form-control" value="{{ $value }}" placeholder="{{ __('Value') }}">
+                                </div>
+                                <button class="col btn btn-sm btn-warning" style="max-width:40px!important;" onClick="remove_claim(this)">X</button>
+                            </div>
+                        @endforeach
+                        </form>
+                    </div>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-sm {{ $user['disabled'] ? 'btn-success' : 'btn-secondary' }}" onClick="setStatus('{{ $user['uid'] }}', {{ $user['disabled'] }})">
-                        {{ $user['disabled'] ? __('Enable') : __('Disable') }}
-                    </button>
-                    <button type="button" class="btn btn-sm btn-primary" onClick="revokeRefreshTokens('{{ $user['uid'] }}')">
-                        {{ __('Revoke Token') }}
-                    </button>
-                    <button type="button" class="btn btn-sm btn-danger" onClick="deleteUser('{{ $user['uid'] }}')">
-                        {{ __('Delete') }}
-                    </button>
+                <div class="modal-footer d-flex justify-content-between">
+                    <div>
+                        <button type="button" class="btn btn-sm btn-dark" onClick="@this.getCustomUserClaims('{{ $user['uid'] }}')">
+                            {{ __('Reload') }}
+                        </button>
+                        <button type="button" class="btn btn-sm btn-secondary" onClick="generate_new_claim()">
+                            {{ __('New') }}
+                        </button>
+                        <button type="button" class="btn btn-sm btn-success" onClick="set_claims('{{ $user['uid'] }}')">
+                            {{ __('Set Claims') }}
+                        </button>
+                    </div>
+                    <div>
+                        <button type="button" class="btn btn-sm {{ $user['disabled'] ? 'btn-success' : 'btn-secondary' }}" onClick="setStatus('{{ $user['uid'] }}', {{ $user['disabled'] }})">
+                            {{ $user['disabled'] ? __('Enable') : __('Disable') }}
+                        </button>
+                        <button type="button" class="btn btn-sm btn-primary" onClick="revokeRefreshTokens('{{ $user['uid'] }}')">
+                            {{ __('Revoke Token') }}
+                        </button>
+                        <button type="button" class="btn btn-sm btn-danger" onClick="deleteUser('{{ $user['uid'] }}')">
+                            {{ __('Delete') }}
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -72,7 +101,12 @@
 </div>
 @push('scripts-firebase-user-list')
 <script>
+
+    let claimsForm;
+
     document.addEventListener('livewire:load', function () {
+
+        claimsForm = document.getElementById('claimsForm');
 
         toLocalDateString();
 
@@ -106,6 +140,41 @@
             return;
         @this.deleteUser(uid);
     }
+
+    function remove_claim(evt) {
+        evt.parentNode.remove();
+    }
+
+    function generate_new_claim() {
+        const template = '<div class="row m-0 p-0 mb-2">\
+            <div class="col m-0 p-0">\
+                <input name="claims_key" class="form-control" placeholder="{{ __('Key') }}">\
+            </div>\
+            <div class="col m-0 p-0 mx-2">\
+                <input name="claims_value" class="form-control" placeholder="{{ __('Value') }}">\
+            </div>\
+            <button class="col btn btn-sm btn-warning" style="max-width:40px!important;" onClick="remove_claim(this)">X</button>\
+        </div>';
+        const node = new DOMParser().parseFromString(template, 'text/html').body.childNodes[0];
+        claimsForm.appendChild(node);
+    }
+
+    function set_claims(uid) {
+        const claims = {};
+        const claimsKeys = document.getElementsByName('claims_key');
+        const claimsValues = document.getElementsByName('claims_value');
+        for (let i=0;i<claimsKeys.length;i++) {
+            if (claimsKeys[i].value != '' && claimsValues[i].value != '') {
+                try {
+                    claims[claimsKeys[i].value] = JSON.parse(claimsValues[i].value);
+                } catch(e) {
+                    claims[claimsKeys[i].value] = claimsValues[i].value;
+                }
+            }
+        }
+        @this.setCustomUserClaims(uid, claims);
+    }
+
 </script>
 @endpush
 @stack('scripts-firebase-user-list')
